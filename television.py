7c842cd50585880e4ae3f77d56a4076324adcc2d
Cbryant2187 <21cbryant87@gmail.com>
2025-08-10 20:43:55 -0500

    Finished docstrings and type hinting in television.py

--- television.py
+++ television.py
@@ -1,22 +1,34 @@
 class Television:
-    MIN_VOLUME = 0
-    MAX_VOLUME = 2
-    MIN_CHANNEL = 0
-    MAX_CHANNEL = 3
+    """
+    A class representing details about a television object.
+    """
+    MIN_VOLUME: int = 0
+    MAX_VOLUME: int = 2
+    MIN_CHANNEL: int = 0
+    MAX_CHANNEL: int = 3
 
-    def __init__(self):
-        self.__status = False
-        self.__muted = False
+    def __init__(self) -> None:
+        """
+        Method to set default values for a television object.
+        """
+        self.__status: bool = False
+        self.__muted: bool = False
         self.__volume = Television.MIN_VOLUME
         self.__channel = Television.MIN_CHANNEL
 
-    def power(self):
+    def power(self) -> None:
+        """
+        Method to set power status of television object.
+        """
         if self.__status:
             self.__status = False
         else:
             self.__status = True
 
-    def mute(self):
+    def mute(self) -> None:
+        """
+        Method to set mute status of television object.
+        """
         if self.__status:
 
             if self.__muted:
@@ -24,7 +36,10 @@ class Television:
             else:
                 self.__muted = True
 
-    def channel_up(self):
+    def channel_up(self) -> None:
+        """
+        Method to set channel of television object one higher.
+        """
         if self.__status:
 
             target_up_channel = self.__channel + 1
@@ -34,7 +49,10 @@ class Television:
             else:
                 self.__channel = target_up_channel
 
-    def channel_down(self):
+    def channel_down(self) -> None:
+        """
+        Method to set channel of television object one lower.
+        """
         if self.__status:
 
             target_down_channel = self.__channel - 1
@@ -44,7 +62,10 @@ class Television:
             else:
                 self.__channel = target_down_channel
 
-    def volume_up(self):
+    def volume_up(self) -> None:
+        """
+        Method to set volume level of television object one higher.
+        """
         if self.__status:
 
             if self.__muted:
@@ -55,7 +76,10 @@ class Television:
             else:
                 self.__volume = self.__volume + 1
 
-    def volume_down(self):
+    def volume_down(self) -> None:
+        """
+        Method to set volume level of television object one lower.
+        """
         if self.__status:
 
             if self.__muted:
@@ -66,15 +90,12 @@ class Television:
             else:
                 self.__volume = self.__volume - 1
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Method to return string containing a television object's current details.
+        :return: A string containing a television object's current details.
+        """
         if self.__muted:
             return f"Power = {self.__status}, Channel = {self.__channel}, Volume = 0"
         else:
             return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
-
-
-
-
-
-
-
