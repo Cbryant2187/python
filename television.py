9b571d77413046d5b0c0bc98472296def96de0a9
Cbryant2187 <21cbryant87@gmail.com>
2025-08-10 20:01:16 -0500

    finished television.py logic

--- television.py
+++ television.py
@@ -1,5 +1,80 @@
-def main():
-    print("Welcome to Television")
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
 
-if __name__ == "__main__":
-    main()
+    def __init__(self):
+        self.__status = False
+        self.__muted = False
+        self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
+
+    def power(self):
+        if self.__status:
+            self.__status = False
+        else:
+            self.__status = True
+
+    def mute(self):
+        if self.__status:
+
+            if self.__muted:
+                self.__muted = False
+            else:
+                self.__muted = True
+
+    def channel_up(self):
+        if self.__status:
+
+            target_up_channel = self.__channel + 1
+
+            if target_up_channel > Television.MAX_CHANNEL:
+                self.__channel = Television.MIN_CHANNEL
+            else:
+                self.__channel = target_up_channel
+
+    def channel_down(self):
+        if self.__status:
+
+            target_down_channel = self.__channel - 1
+
+            if target_down_channel < Television.MIN_CHANNEL:
+                self.__channel = Television.MAX_CHANNEL
+            else:
+                self.__channel = target_down_channel
+
+    def volume_up(self):
+        if self.__status:
+
+            if self.__muted:
+                self.__muted = False
+
+            if self.__volume == Television.MAX_VOLUME:
+                pass
+            else:
+                self.__volume = self.__volume + 1
+
+    def volume_down(self):
+        if self.__status:
+
+            if self.__muted:
+                self.__muted = False
+
+            if self.__volume == Television.MIN_VOLUME:
+                pass
+            else:
+                self.__volume = self.__volume - 1
+
+    def __str__(self):
+        if self.__muted:
+            return f"Power = {self.__status}, Channel = {self.__channel}, Volume = 0"
+        else:
+            return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
+
+
+
+
+
+
+
